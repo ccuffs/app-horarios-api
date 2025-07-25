1758c6f791e5ae03f5be60baa01503c57223f34b
giancds <gian.salton@gmail.com>
2025-07-25 18:10:34 -0300

    Alterado schema do modelo ano-semestre.

--- src/models/ano-semestre.js
+++ src/models/ano-semestre.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
 		{
 			sequelize,
 			tableName: "ano_semestre",
-			schema: "horarios",
+			schema: "public",
 			freezeTableName: true,
 			timestamps: false,
 		},
